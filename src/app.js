b3dd939aa96f9312869c26787ca9757209811eca
Omolola <adefilaomolola@gmail.com>
2023-09-17 01:25:15 -0800

    Worked on getting date of the city

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
+function formatDate (timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  return `${day} ${hours}: ${minutes}`;
+}
+
+
+
+
+
 function displayTemperature (response) {
     console.log(response.data)
     let cityElement = document.querySelector("#city");
     let temperatureElement = document.querySelector("#temperature");
     let descriptionElement = document.querySelector("#description");
     let windElement = document.querySelector("#wind");
-    let humidityElement = document.querySelector("#humidity")
+    let humidityElement = document.querySelector("#humidity");
+    let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = (response.data.city);
     descriptionElement.innerHTML = (response.data.condition.description);
     windElement.innerHTML =Math.round(response.data.wind.speed);
     humidityElement.innerHTML = (response.data.temperature.humidity);
+    dateElement.innerHTML = formatDate(response.data.time*1000);
 }
 
 
