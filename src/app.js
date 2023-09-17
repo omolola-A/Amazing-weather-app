f28030711a4d07de3425e543f36d25e8151cbb1e
Omolola <adefilaomolola@gmail.com>
2023-09-17 02:01:28 -0800

    Worked on getting the actual day of the week.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 function formatDate (timeStamp) {
   let date = new Date(timeStamp);
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
-  let day = date.getDay();
-  return `${day} ${hours}: ${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday", 
+    "Monday",
+     "Tuesday", 
+     "Wednesday", 
+     "Thursday", 
+     "Friday", 
+     "Saturday"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-
-
-
 
 
 function displayTemperature (response) {
