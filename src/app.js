51534164981d4ad2acdf7be80ce3d33649afaba9
Omolola <adefilaomolola@gmail.com>
2023-09-28 14:06:19 -0800

    Removed the imperial unit conversion✌

--- src/app.js
+++ src/app.js
@@ -114,33 +114,8 @@ function handleSubmit (event) {
   console.log(cityInputElement.value);
   search(cityInputElement.value);
 }
-let celsiusTemperature = null
-
-function displayFahrenheitTemperature (event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celsiuslink.classList.remove("active");
-  fahrenheitlink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
- function displayCelsiusTemperature (event) {
-  event.preventDefault();
-  celsiuslink.classList.add("active");
-  fahrenheitlink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
- }
-
 
   let form = document.querySelector("#search-form");
   form.addEventListener("submit", handleSubmit);
-
-
-   let fahrenheitlink = document.querySelector("#fahrenheit-link");
-   fahrenheitlink.addEventListener("click", displayFahrenheitTemperature);
-
-   let celsiuslink = document.querySelector("#celsius-link");
-   celsiuslink.addEventListener("click", displayCelsiusTemperature);
 
     search("New York");
